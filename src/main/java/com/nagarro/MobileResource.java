7c06fd751f80cecf5be28b15257455228aba6742
avinashkate <avinashkate44@gmail.com>
2023-09-05 13:47:02 +0530

    Remove unused imports

--- src/main/java/com/nagarro/MobileResource.java
+++ src/main/java/com/nagarro/MobileResource.java
@@ -5,7 +5,6 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
